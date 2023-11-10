e24c56c907f2bede775dbef245c54a499c481a6c
krisnaxyoga <yogakris.yk@gmail.com>
2023-11-10 21:22:51 +0700

    table baru

---
+++ app/Models/ThemesSell.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ThemesSell extends Model
+{
+    use HasFactory;
+}
